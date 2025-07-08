660a53de4d6aff9d2e4e8912372c8d3a0f9f5bd0
Henri861 <hdantas390@gmail.com>
2025-07-08 17:41:12 -0300

    Create Funções báscias para números.py

---
+++ Funções báscias para números.py
@@ -0,0 +1,15 @@
+num1 = 3.75444
+print(num1)
+print(round(num1))
+print(round(num1, 3))
+
+# (round) arredonda o número decimal para um número inteiro
+
+'''print(pow(2,3)) #(pow) é potenciação
+
+print(max(1,8,5,17,22,3,45,11)) #(max) mostra qual é o maior número da sequência
+
+print(min(4,5,7,1,9,55)) # (min) exibe o menor número da sequência
+
+print(sum([3,7,8])) #(sum) soma todos os números da sequência, mas com colchetes[]
+'''
